94bfa64c4250089c77c0c32ad4da0423808c3e50
ayush3160 <kshitij3160@gmail.com>
2024-11-13 12:14:25 +0530

    initial commit
    Signed-off-by: ayush3160 <kshitij3160@gmail.com>

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/http", async (req, res) => {
+  try {
+    const axiosRes = await axios.get("https://api.github.com/users/ayush3160");
+
+    res.send(axiosRes.data);
+  } catch (error) {
+    res.send(error);
+    console.error(error);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
